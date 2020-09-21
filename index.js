20c252c4599702bc8a257b7388e715a875ced443
devanshi-katyal <devanshi.katyal09@gmail.com>
2020-09-21 20:02:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//sound by pWressing the button
+var myAudio = document.getElementById("myAudio");
+var isPlaying = false;
+
+function togglePlay() {
+  isPlaying ? myAudio.pause() : myAudio.play();
+};
+
+myAudio.onplaying = function() {
+  isPlaying = true;
+};
+myAudio.onpause = function() {
+  isPlaying = false;
+};
